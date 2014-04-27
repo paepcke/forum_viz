ca46234617d75ac6a7d30f792508e905fb53ef9f
Akshay Agrawal <akshaykagrawal7@gmail.com>
2014-04-26 22:33:04 -0700

    Basic framework for dumping posts from given course

---
+++ src/nlp/dump_course_post_text.py
@@ -0,0 +1,20 @@
+import mysqldb
+import getpass
+
+
+def main():
+	password = getpass.getpass("Password: ")
+	outfile = raw_input("Output file prefix: ")
+	course_name = raw_input("Course name: ")	
+	db = mysqldb.MySQLDB('datastage.stanford.edu', user='akshayka', passwd=password, db='EdxPrivate')
+	file_suffix = 0
+	for result in db.query(
+		'select body from ForumRaw where course_display_name="' + \
+			course_name + '" limit 10'):
+		result_str = str(result)
+		f.write(result_str[result_str.index('\'') + 1:result_str.rindex('\'')].strip('\n'))
+		f.close()
+		file_suffix += 1
+
+if __name__ == "__main__":
+    main()
