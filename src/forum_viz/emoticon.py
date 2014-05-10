4fc003b0052879f89b362b8e7dcc34ac625da40e
Akshay Agrawal <akshaykagrawal7@gmail.com>
2014-05-10 01:16:22 -0700

    Restructure Repository
    Restructure the directory organization; add the emoticon.py tool
    that retrieves training data for sentiment. Add sentiment training
    data files. (NB: training data not yet formatted / cleaned.)

---
+++ src/forum_viz/emoticon.py
@@ -0,0 +1,95 @@
+import argparse
+from forum import Forum
+
+default_course_name = 'Medicine/HRP258/Statistics_in_Medicine'
+table = 'contents'
+attr = ['anon_screen_name', 'type', 'anonymous', 'anonymous_to_peers', \
+		'body', 'created_at', 'votes', 'down_count', 'up_count', \
+			'comment_thread_id', 'parent_id', 'parent_ids']
+fields = ','.join(attr)
+happy_emoticons = '.*(:-?|=|;)[])DP].*'
+sad_emoticons = '.*(:-?|=)[[(].*'
+all_emoticons = '.*(:-?|=|;)[])DP[(].*'
+
+def write_posts_to_file(f, posts):
+	count = 0
+	for post in posts:
+		f.write(str(count) + ':<' + post.body + '>\n')
+		count += 1
+
+def write_all_training(forum, path):
+	# Get list of all course names
+	courses = forum.course_names()
+	for course in courses:
+		print 'Fetching posts for course ' + course
+		pos_posts = forum.post_contents(attr, \
+			query='select ' + fields + ' from ' + table + \
+				' where course_display_name="' + course + '" AND '\
+					'body rlike "' + happy_emoticons + '"')
+		neg_posts = forum.post_contents(attr, \
+			query='select ' + fields + ' from ' + table + \
+				' where course_display_name="' + course + '" AND '\
+					'body rlike "' + sad_emoticons + '"')
+		pos_len = len(pos_posts)
+		neg_len = len(neg_posts)
+		if (pos_len + neg_len > 0):
+			course = course.replace('/', '_')
+			try:
+				f_pos = open(path + 'positive/' + course + '_' + \
+					str(pos_len) + '_pos_tr', 'w')
+				f_neg = open(path + 'negative/' + course + '_' + \
+					str(neg_len) + '_neg_tr', 'w')
+			except IOError:
+				print 'failed to open file for course ' + course
+				return
+			print 'Writing posts for course ' + course
+			write_posts_to_file(f_pos, pos_posts)
+			write_posts_to_file(f_neg, neg_posts)
+			f_pos.close()
+			f_neg.close()
+			
+def print_course_emoticons(forum, course_name, regex):
+	emoticon_posts = forum.post_contents(attr,
+		query='select ' + fields + ' from ' + table + \
+			' where course_display_name="' + course_name + '" AND '\
+				'body rlike "' + regex + '"')
+
+	count = 0
+	for post in emoticon_posts:
+		print str(count) + ':<' + post.body + '>'
+		count += 1
+
+def main():
+	# Args: course name and emotion
+	parser = argparse.ArgumentParser(description='Simple tool to fetch ' \
+		'posts with emoticons.')
+	parser.add_argument('-cn', '--coursename', type=str,
+		help='name of course; defaults to' + default_course_name)
+	parser.add_argument('-em', '--emotion', type=str,
+		help='one of \'happy\', \'sad\', \'all\'; defaults to \'all\'.')
+	parser.add_argument('-ac', '--allcourses', type=str,
+		help='updates all training data, path to sentiment directory (include trailing /')
+	args = parser.parse_args()
+
+	forum = Forum()
+	if args.allcourses is not None:
+		write_all_training(forum, args.allcourses)
+	else:
+		if args.coursename is None:
+			course_name = default_course_name
+		else:
+			course_name = args.coursename
+
+		if args.emotion is None:
+			regex = all_emoticons
+		elif args.emotion == 'happy':
+			regex = happy_emoticons
+		elif args.emotion == 'sad':
+			regex = sad_emoticons
+		else:
+			regex = all_emoticons
+		
+		print_course_emoticons(forum, course_name, regex)
+
+if __name__ == "__main__":
+	main()
