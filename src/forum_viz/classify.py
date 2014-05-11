96a4d38447f1ed82e84a46639d2e82ce8b6b027d
Akshay Agrawal <akshaykagrawal7@gmail.com>
2014-05-10 22:47:55 -0700

    Create util file
    Adds util.py, a module to hold convenient utility functions related
    to the task of classification.

--- src/forum_viz/classify.py
+++ src/forum_viz/classify.py
@@ -1,29 +1,7 @@
-import pickle
 from nltk.classify import naivebayes, accuracy
 from nltk.classify.util import LazyMap
 from subprocess import Popen, PIPE, STDOUT
-
-# Takes a string representation of a single discussion
-# forum post and returns the bucket into which it was placed.
-path_to_corenlp_jars = \
-	'../lib/corenlp-python/stanford-corenlp-full-2013-11-12/*'
-path_to_corenlp_sentiment = \
-		'edu.stanford.nlp.sentiment.SentimentPipeline'
-
-def unpickle_file(path_to_file):
-	# Open the file for unpickling.
-	try:
-		f = open(path_to_file, 'rb')
-	except IOError:
-		return None
-	# Unpickle each entry in the file and inflate our list
-	posts = []
-	while True:
-		try:
-			posts.append(pickle.load(f))
-		except EOFError:
-			break
-	return posts
+import util
 
 class PostClassifier:
 	
@@ -36,7 +14,7 @@ class PostClassifier:
 		return 'education'
 	def classify_sentiment_train(self, path_to_labeled_data):
 		# 1) Unpickle Data
-		posts = unpickle_file(path_to_labeled_data)
+		posts = util.unpickle_file(path_to_labeled_data)
 
 		# 2) Eliminate Noise Words and Inflate sentiment_feature_words
 		self.sentiment_features = self.build_sentiment_features(posts)
@@ -48,9 +26,8 @@ class PostClassifier:
 		self.sentiment_classifier = \
 			naivebayes.NaiveBayesClassifier.train(training_set)	
 	def classify_sentiment_test(self, path_to_testing_data):
-		# TODO: Take two -- NLTK Classification
 		# 1) Unpickle data
-		posts = unpickle_file(path_to_testing_data)
+		posts = util.unpickle_file(path_to_testing_data)
 		# 2) Extract features
 		testing_set = self.lazy_apply_feautres(posts)
 		# 3) Test with classifier
@@ -63,12 +40,16 @@ class PostClassifier:
 			guess = self.sentiment_classifier.classify(features)
 			if guess != sentiment:
 				errors.append((guess, sentiment, post))
-		# 4) Print accuracy / recall / precision / etc.
 		return (test_accuracy, errors)
 
 	def classify_sentiment(self, sentence):
 		features = self.extract_features(sentence.split())
 		return self.sentiment_classifier.classify(features)
+
+	path_to_corenlp_jars = \
+		'../lib/corenlp-python/stanford-corenlp-full-2013-11-12/*'
+	path_to_corenlp_sentiment = \
+		'edu.stanford.nlp.sentiment.SentimentPipeline'
 
 	def classify_sentiment_core_nlp(self, sentences):
 		# A basic implementation that uses the stanford sentiment classifier
@@ -90,7 +71,6 @@ class PostClassifier:
 		def lazy_func(labeled_token):
 			return (self.extract_features(labeled_token[0]), labeled_token[1])
 		return LazyMap(lazy_func, toks)
-
 
 	#TODO Filter out stop words 
 	@staticmethod
